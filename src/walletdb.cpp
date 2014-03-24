60a46643a252e7a4a9e57e14ec257c82d433e9af
supermoose <cesmall08@gmail.com>
2014-03-24 20:35:55 +0000

    Update walletdb.cpp

--- src/walletdb.cpp
+++ src/walletdb.cpp
@@ -469,7 +469,7 @@ DBErrors CWalletDB::LoadWallet(CWallet* pwallet)
 void ThreadFlushWalletDB(const string& strFile)
 {
     // Make this thread recognisable as the wallet flushing thread
-    RenameThread("bitcoin-wallet");
+    RenameThread("votecoin-wallet");
 
     static bool fOneThread;
     if (fOneThread)
